400a7057314aec75cfcc2c8ecf13cfe5ac2d8c2e
Estera Bechian <esterabechian12@hotmail.com>
2024-08-10 14:02:26 +0000

    Added the string characters

--- app.js
+++ app.js
@@ -7,8 +7,34 @@ const lowercaseElement = document.querySelector('#lowercase');
 const numbersElement = document.querySelector('#number');
 const symbolsElement = document.querySelector('#symbols');
 
-// reference to the button and the copy 
+// reference to the button and the clipboard
 const generateBtn = document.querySelector('#generate');
 const copyBtnElement = document.querySelector('#copy');
 
+// the strings of character
 
+const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'
+const numbersChars = '0123456789';
+const symbolsChars = '!#@&%*(){[$'
+
+
+function generatePassword() {
+    // refrence to the password we gonna generate
+    let password = "";
+    let length = lengthElement.value;
+    let characters = "";
+
+    // if this is true then we add the respective chars otherwise it an empty string
+    characters += uppercaseElement.checked ? uppercaseChars : "";
+    characters += lowercaseElement.checked ? lowercaseChars : "";
+    characters += numbersElement.checked ? numbersChars : "";
+    characters += symbolsElement.checked ? symbolsChars : "";
+    
+
+    
+
+
+
+
+}
