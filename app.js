d029c40fe2da0a2d01ed15a2903cb4602a41dce9
Estera Bechian <esterabechian12@hotmail.com>
2024-08-10 15:11:43 +0000

    added functionality to the button and the clipboard

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 // password input
-const passwordElement = document.querySelector('#password');
+const passwordElement = document.querySelector("#password");
 // reference to every check boxes
-const lengthElement = document.querySelector('#length');
-const uppercaseElement = document.querySelector('#uppercase');
-const lowercaseElement = document.querySelector('#lowercase');
-const numbersElement = document.querySelector('#number');
-const symbolsElement = document.querySelector('#symbols');
+const lengthElement = document.querySelector("#length");
+const uppercaseElement = document.querySelector("#uppercase");
+const lowercaseElement = document.querySelector("#lowercase");
+const numbersElement = document.querySelector("#numbers");
+const symbolsElement = document.querySelector("#symbols");
 
 // reference to the button and the clipboard
-const generateBtn = document.querySelector('#generate');
-const copyBtnElement = document.querySelector('#copy');
+const generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener('click', generatePassword);
+
+
+const copyBtnElement = document.querySelector("#copy");
+copyBtnElement.addEventListener('click', copyPassword)
 
 // the strings of character
 
-const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'
-const numbersChars = '0123456789';
-const symbolsChars = '!#@&%*(){[$'
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const numbersChars = "0123456789";
+const symbolsChars = "!#@&%*(){[$";
 
 
 function generatePassword() {
@@ -31,10 +35,26 @@ function generatePassword() {
     characters += numbersElement.checked ? numbersChars : "";
     characters += symbolsElement.checked ? symbolsChars : "";
     
+   for (let i = 0; i <= length; i++) {
+         let randomPassword = Math.floor(Math.random()  * characters.length);
+         // this will cut  the string at that character and added to the password
+         password += characters.substring(randomPassword, randomPassword + 1);
+   }
 
-    
-
-
-
+   passwordElement.value = password;
 
 }
+
+
+
+
+async function copyPassword() {
+   if (navigator.clipboard) {
+      await navigator.clipboard.writeText(passwordElement.value);
+      alert("Password copied to clipboard")
+   }
+}
+
+
+
+
