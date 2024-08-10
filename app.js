29676df697c2acf5e71ca75fb6f53a80865ed3a0
Estera Bechian <esterabechian12@hotmail.com>
2024-08-10 13:29:13 +0000

    Selected the refrences to the elements for every checkboxes

---
+++ app.js
@@ -0,0 +1,14 @@
+// password input
+const passwordElement = document.querySelector('#password');
+// reference to every check boxes
+const lengthElement = document.querySelector('#length');
+const uppercaseElement = document.querySelector('#uppercase');
+const lowercaseElement = document.querySelector('#lowercase');
+const numbersElement = document.querySelector('#number');
+const symbolsElement = document.querySelector('#symbols');
+
+// reference to the button and the copy 
+const generateBtn = document.querySelector('#generate');
+const copyBtnElement = document.querySelector('#copy');
+
+
